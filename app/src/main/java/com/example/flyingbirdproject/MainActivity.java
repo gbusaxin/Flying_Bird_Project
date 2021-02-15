706566e3b8282b9fd75d918cca2c2d3529f644c4
grigorijbusahin <gbusaxin@gmail.com>
2021-02-15 18:13:23 +0200

    Monday

--- app/src/main/java/com/example/flyingbirdproject/MainActivity.java
+++ app/src/main/java/com/example/flyingbirdproject/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.flyingbirdproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView bird, enemy1, enemy2, enemy3, coin;
+    private Button buttonStart;
+    private Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bird = findViewById(R.id.main_bird);
+        enemy1 = findViewById(R.id.enemy1);
+        enemy2 = findViewById(R.id.enemy2);
+        enemy3 = findViewById(R.id.enemy3);
+        coin = findViewById(R.id.coin);
+        buttonStart = findViewById(R.id.buttonStart);
+
+        animation = AnimationUtils.loadAnimation(this,R.anim.scale_animation);
+        bird.setAnimation(animation);
+        enemy1.setAnimation(animation);
+        enemy2.setAnimation(animation);
+        enemy3.setAnimation(animation);
+        coin.setAnimation(animation);
+    }
+
+    public void onGameActivityStart(View view){
+        startActivity(new Intent(this, GameActivity.class));
+        finish();
     }
 }
