7c9cbaafd23b84ea79a3b159501a8c1080f9fbd9
grigorijbusahin <gbusaxin@gmail.com>
2021-02-16 10:53:22 +0200

    tuesday

--- app/src/main/java/com/example/flyingbirdproject/GameActivity.java
+++ app/src/main/java/com/example/flyingbirdproject/GameActivity.java
@@ -1,14 +1,72 @@
 package com.example.flyingbirdproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
+    private ImageView bird, enemy1, enemy2, enemy3, coin1, coin2, right1, right2, right3;
+    private TextView textViewScore, textViewStartInfo;
+    private ConstraintLayout constraintLayout;
+    private boolean touchControl = false;
+    private boolean beginControl = false;
+    private Runnable runnable;
+    private Handler handler;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        bird = findViewById(R.id.imageViewBird);
+        enemy1 = findViewById(R.id.imageViewEnemy1);
+        enemy2 = findViewById(R.id.imageViewEnemy2);
+        enemy3 = findViewById(R.id.imageViewEnemy3);
+        coin1 = findViewById(R.id.imageViewCoin);
+        coin2 = findViewById(R.id.imageViewCoin2);
+        right1 = findViewById(R.id.right1);
+        right2 = findViewById(R.id.right2);
+        right3 = findViewById(R.id.right3);
+        textViewScore = findViewById(R.id.textViewScore);
+        textViewStartInfo = findViewById(R.id.textViewStartInfo);
+        constraintLayout = findViewById(R.id.constraintLayout);
+
+        constraintLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                textViewStartInfo.setVisibility(View.INVISIBLE);
+
+                if (!beginControl) {
+                    beginControl = true;
+                    handler = new Handler();
+                    runnable = new Runnable() {
+                        @Override
+                        public void run() {
+                            handler.postDelayed(runnable, 20);
+                        }
+                    };
+                    handler.post(runnable);
+                } else {
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        touchControl = true;
+                    }
+
+                    if (event.getAction() == MotionEvent.ACTION_UP) {
+                        touchControl = false;
+                    }
+                }
+                return true;
+            }
+        });
     }
 }
